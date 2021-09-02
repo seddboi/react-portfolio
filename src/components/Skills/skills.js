15bb56a968a629bf52c3d894d782c300b70f4605
Gian Zamora <gz2016@att.net>
2021-09-02 16:31:49 -0700

    added skills sections and adjusted some minor effects on previous sections

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -3,15 +3,32 @@ import './skills.css';
 
 function Skills() {
     return (
-        <div>
-            <h2>My Strengths</h2>
-            <ul>
-                <li>React.js</li>
-                <li></li>
-                <li></li>
-                <li></li>
+        <div class='grid-x align-spaced'>
+            <div class='cell small-3 text-center' id='card'>
+                <h3><strong>Client Side Strengths</strong></h3>
+                <div id='centered-list-1'>
+                    <ul id='list'>
+                        <li><strong>React.js</strong></li>
+                        <li><strong>HTML5</strong></li>
+                        <li><strong>CSS / Bootstrap / Foundation</strong></li>
+                        <li><strong>Version Control / Git / Command Line</strong></li>
+                    </ul>
+                </div>
+            </div>
 
-            </ul>
+            <div class='cell small-3 text-center' id='card' >
+                <h3><strong>Server Side Strengths</strong></h3>
+                <div id='centered-list-2'>
+                    <ul id='list'>
+                        <li><strong>Node</strong></li>
+                        <li><strong>Express</strong></li>
+                        <li><strong>MySQL</strong></li>
+                        <li><strong>Sequelize</strong></li>
+                    </ul>
+                </div>
+                
+            </div>
+            
         </div>
     )
 };
--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './wrapper.css';
 import MainHeader from '../MainHeader/mainheader';
 import Bio from '../Bio/bio';
+import Skills from '../Skills/skills';
 
 function Wrapper() {
     return (
         <div>
             <MainHeader />
             <Bio />
+            <Skills />
         </div>
     )
 }
