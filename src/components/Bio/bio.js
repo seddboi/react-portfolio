da4cf4d656b5bf4c29bb01a3c07d6c4faed3b399
Gian Zamora <gz2016@att.net>
2021-09-14 01:21:42 -0700

    added scroll animation queue to bio

--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './bio.css'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 function Bio() {
     return (
-        <div class='grid-x align-center' id='top-layer'>
-            <div class='callout large' id='about-me'>
-            <p><strong>Hi, </strong></p>
-            <p><strong>I am a recent University of California: Riverside Graduate with a degree in Applied Mathematics, 
-                and am looking to further explore my strengths as a learner and as an achiever. Despite having a background in Mathematics, 
-                I have spent countless hours building my strengths in developing my own web based applications! 
-                With my urge to provide quality applications and my drive to work a fast paced environment and meet deadlines, I am sure to
-                be a great fit in any position I am placed in!</strong> </p>
+        <ScrollAnimation animateIn='bio-drop'>
+            <div class='grid-x align-center' id='top-layer'>
+                <div class='callout large' id='about-me'>
+                    <p><strong>Hi, </strong></p>
+                    <p><strong>I am a recent University of California: Riverside Graduate with a degree in Applied Mathematics, 
+                        and am looking to further explore my strengths as a learner and as an achiever. Despite having a background in Mathematics, 
+                        I have spent countless hours building my strengths in developing my own web based applications! 
+                        With my urge to provide quality applications and my drive to work a fast paced environment and meet deadlines, I am sure to
+                        be a great fit in any position I am placed in!</strong> </p>
+                    </div>
             </div>
-        </div>
-        
+        </ScrollAnimation>
     )
 };
 export default Bio;
