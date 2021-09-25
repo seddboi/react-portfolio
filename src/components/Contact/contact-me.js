9e069738784dcc5d2216e3d95e41ae4641431bf0
Gian Zamora <gz2016@att.net>
2021-09-24 22:38:12 -0700

    fixed box shadow effect

---
+++ src/components/Contact/contact-me.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './contact-me.css';
+
+function Contact() {
+    return (
+        <div>
+            <div class='callout row'>
+                <img src='./selfie/IMG_1892_2.jpg'/>
+                <div>
+                    <h6>Email: zamorag@gmail.com</h6>
+                    <h6></h6>
+                    <h6></h6>
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Contact;
