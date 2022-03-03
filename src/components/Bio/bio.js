c253388984fc72c278cf3da94097de906fbc1060
Gian Zamora <gz2016@att.net>
2022-03-02 17:24:18 -0800

    added parallax with dif pic as header

--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -7,7 +7,7 @@ function Bio() {
         <ScrollAnimation animateIn='fade-in'>
             <div class='grid-x align-center' id='top-layer'>
                 <div class='callout large' id='about-me'>
-                    <p><strong>Hi, </strong></p>
+                    <p><strong>Hi, I'm Gian Zamora. </strong></p>
                     <p><strong>I'm a Full Stack Developer focusing primarily on contemporary design in web-based applications. Recently earned a certificate 
                         in full stack web development from UC Riverside Extension, attaining skills like Javascript, Node.js, MySQL and React.js. Known to be a pragmatic 
                         and inventive designer, focusing primarily on responsive React designs. As a great team player and an intuitive problem-solver, I am excited to provide quality applications 
