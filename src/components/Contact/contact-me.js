1cfe0fbfa6eb82914fa39dae43476eac40be4417
Gian Zamora <gz2016@att.net>
2022-07-18 12:30:18 -0700

    adjusted projects and replaced old with new

--- src/components/Contact/contact-me.js
+++ src/components/Contact/contact-me.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import './contact-me.css';
 
-import selfie from './selfie/portfolio.jpeg'
-import github from './selfie/GitHub-Mark-64px.png'
-import linkedin from './selfie/LI-In-Bug.png'
-import resume from './selfie/wr-resumes-logo.png'
+import selfie from './selfie/portfolio.jpeg';
+import github from './selfie/GitHub-Mark-64px.png';
+import linkedin from './selfie/LI-In-Bug.png';
+import resume from './selfie/wr-resumes-logo.png';
 
 function Contact() {
-    const clipboardCopy = () => {
-        var copyText = document.getElementById('myInput');
+	const clipboardCopy = () => {
+		var copyText = document.getElementById('myInput');
 
-        copyText.select();
-        copyText.setSelectionRange(0,99999);
+		copyText.select();
+		copyText.setSelectionRange(0, 99999);
 
-        document.execCommand("copy");
-        alert('Email Copied to Clipboard!');
-    };
+		document.execCommand('copy');
+		alert('Email Copied to Clipboard!');
+	};
 
-    return ( 
-        <div class='contact-div' id='centered-title'>
-            <h1 id='contact-title'>Contact Me</h1>
-            <div class='callout' id='contact-section'>
-                <img id='selfie' alt='picture of the sexy beast, Gian Zamora' src={selfie} />
-                <div class='bottom-text'>
-                    <input type='text' value='zmr.gian@gmail.com' id='myInput'/>
-                    <button type='button' class='clipboard-button' onClick={clipboardCopy}>Copy Email</button>
-                    <div class='row-links'>
-                        <a href='https://github.com/seddboi' target='_blank'><img class='links' src={github} alt='Github Link'/></a>
-                        <a href='https://www.linkedin.com/in/gian-zamora/' target='_blank'><img class='links' id='linkedin' src={linkedin} alt='LinkedIn Link'/></a>
-                        <a href='https://docs.google.com/document/d/13GkBST-zhbndjGA2Q9p30eVImBoOIslvF4NYbpHhWOg/edit?usp=sharing' target='_blank'><img class='links' src={resume} alt='Resume Download'/></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-};
+	return (
+		<div class="contact-div" id="centered-title">
+			<h1 id="contact-title">Contact Me</h1>
+			<div class="callout" id="contact-section">
+				<img id="selfie" alt="picture of the sexy beast, Gian Zamora" src={selfie} />
+				<div class="bottom-text">
+					<input type="text" value="zmr.gian@gmail.com" id="myInput" />
+					<button type="button" class="clipboard-button" onClick={clipboardCopy}>
+						Copy Email
+					</button>
+					<div class="row-links">
+						<a href="https://github.com/seddboi" target="_blank">
+							<img class="links" src={github} alt="Github Link" />
+						</a>
+						<a href="https://www.linkedin.com/in/gian-zamora/" target="_blank">
+							<img class="links" id="linkedin" src={linkedin} alt="LinkedIn Link" />
+						</a>
+						<a
+							href="https://docs.google.com/document/d/13GkBST-zhbndjGA2Q9p30eVImBoOIslvF4NYbpHhWOg/edit?usp=sharing"
+							target="_blank"
+						>
+							<img class="links" src={resume} alt="Resume Download" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default Contact;
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -7,59 +7,100 @@ import passwordgenerator from './gif/Password Generator.gif';
 import weathered from './gif/Weathered.gif';
 
 function Projects() {
-    return (
-        <div class='projects-div'>
-            <h2 id='projects-title'><strong>Projects</strong></h2>
-            <div class='callout large row ' id='projects-container'>
-                <div>
-                    <a href='https://seddboi.github.io/Save-A-Shot/'>
-                        <div class='card p1' id='project-card'>
-                            <img id='gif' alt='gif image of Save-A-Shot' src={saveshot} />
-                            <div id='filter'>
-                                <p id='card-text'><span id='main-title'>Save-A-Shot</span><br/> Vaccination Tracker and Vaccination Informational, pending the early stages of the COVID 19 Outbreak</p>
-                                <p id='tech-text'><span id="tech-title">Technologies Used:</span><br/>VaccinateCA API<br/>OpenStreetMap API<br/>Foundation by Zurb Framework<br/> </p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                
-                <div>
-                    <a href='https://seddboi.github.io/Thought-Machine/'>
-                        <div class='card p2' id='project-card'>
-                            <img id='gif' alt='gif image of Thought Machine' src={thoughtmachine} />
-                            <div id='filter'>
-                                <p id='card-text'><span id='main-title'>Thought Machine</span><br/> Simple Passing Thoughts application for all of your thoughts not meant to be publicly shared!</p>
-                                <p id='tech-text'><span id="tech-title">Technologies Used:</span><br/> React<br/>Javascript( Moment.js, React Hooks )<br/>HTML & CSS</p>
-                            </div>                
-                        </div>
-                    </a>
-                </div>  
-                
-                <div>
-                    <a href='https://seddboi.github.io/Random-Password-Creator/'>
-                        <div class='card p3' id='project-card'>
-                            <img id='gif' alt='gif image of Password Generator' src={passwordgenerator} />
-                            <div id='filter'>
-                                <p id='card-text'><span id='main-title'>Password Generator</span><br/> Upper Case, Lower Case, Numbers, and Special Characters... this generator has it all!</p>
-                                <p id='tech-text'><span id="tech-title">Technologies Used:</span><br/> Javascript<br/> HTML & CSS</p>
-                            </div>                
-                        </div>
-                    </a>
-                </div>
-                
-                <div>
-                    <a href='https://weathrd.herokuapp.com/'>
-                        <div class='card p3' id='project-card'>
-                            <img id='gif' alt='gif image of Weathered' src={weathered} />
-                            <div id='filter'>
-                                <p id='card-text'><span id='main-title'>Weathered</span><br/> The one stop weather application, for any of your mobile or desktop needs!</p>
-                                <p id='tech-text'><span id="tech-title">Technologies Used:</span><br/> React JS<br/> Animate.css <br/>FreeWeatherAPI</p>
-                            </div>                
-                        </div>
-                    </a>
-                </div>
+	return (
+		<div class="projects-div">
+			<h2 id="projects-title">
+				<strong>Projects</strong>
+			</h2>
+			<div class="callout large row " id="projects-container">
+				<div>
+					<a href="https://weathrd.herokuapp.com/">
+						<div class="card p3" id="project-card">
+							<img id="gif" alt="gif image of Weathered" src={weathered} />
+							<div id="filter">
+								<p id="card-text">
+									<span id="main-title">Weathered</span>
+									<br /> The one stop weather application, for any of your mobile or desktop needs!
+								</p>
+								<p id="tech-text">
+									<span id="tech-title">Technologies Used:</span>
+									<br /> React
+									<br /> Animate.css <br />
+									FreeWeatherAPI
+								</p>
+							</div>
+						</div>
+					</a>
+				</div>
 
-                {/* <div class='card p5' id='project-card'>
+				<div>
+					<a href="https://seddboi.github.io/Thought-Machine/">
+						<div class="card p2" id="project-card">
+							<img id="gif" alt="gif image of Thought Machine" src={thoughtmachine} />
+							<div id="filter">
+								<p id="card-text">
+									<span id="main-title">Thought Machine</span>
+									<br /> Simple Passing Thoughts application for all of your thoughts not meant to
+									be publicly shared!
+								</p>
+								<p id="tech-text">
+									<span id="tech-title">Technologies Used:</span>
+									<br /> React
+									<br />
+									Javascript( Moment.js, React Hooks )<br />
+									HTML & CSS
+								</p>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div>
+					<a href="https://seddboi.github.io/passwordinator/">
+						<div class="card p3" id="project-card">
+							<img id="gif" alt="gif image of Password Generator" src={passwordgenerator} />
+							<div id="filter">
+								<p id="card-text">
+									<span id="main-title">Password Generator</span>
+									<br /> Here for all of your random password making needs!
+								</p>
+								<p id="tech-text">
+									<span id="tech-title">Technologies Used:</span>
+									<br /> React
+									<br /> Javascript
+									<br /> Material UI
+								</p>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div>
+					<a href="https://seddboi.github.io/Save-A-Shot/">
+						<div class="card p1" id="project-card">
+							<img id="gif" alt="gif image of Save-A-Shot" src={saveshot} />
+							<div id="filter">
+								<p id="card-text">
+									<span id="main-title">Save-A-Shot</span>
+									<br /> Vaccination Tracker and Vaccination Informational, pending the early stages
+									of the COVID 19 Outbreak
+								</p>
+								<p id="tech-text">
+									<span id="tech-title">Technologies Used:</span>
+									<br />
+									VaccinateCA API
+									<br />
+									OpenStreetMap API
+									<br />
+									Foundation by Zurb Framework
+									<br />{' '}
+								</p>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				{/* <div class='card p5' id='project-card'>
                 <img id='gif' src={saveshot} />
                     <div id='filter'>
                         <p id='card-text'>Save-A-Shot</p>
@@ -72,10 +113,9 @@ function Projects() {
                         <p id='card-text'>Save-A-Shot</p>
                     </div>                
                 </div> */}
-                
-            </div>
-        </div>
-    )
-};
+			</div>
+		</div>
+	);
+}
 
 export default Projects;
