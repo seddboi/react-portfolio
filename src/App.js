182274b687e10dab05f23c6d62296e6ab52005e9
Gian Zamora <gz2016@att.net>
2023-01-01 18:04:43 -0800

    removed unnecessary wrapper component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Wrapper from './components/Wrapper/wrapper';
+import MainHeader from './components/MainHeader/mainheader';
+import Bio from './components/Bio/bio';
+import Skills from './components/Skills/skills';
+import Projects from './components/Projects/projects';
+import Contact from './components/Contact/contact-me';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Wrapper />
+      <MainHeader />
+      <Bio />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
