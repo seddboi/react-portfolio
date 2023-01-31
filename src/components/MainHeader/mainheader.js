ea4804cc2d44584951469be0292ce2d77e7e7b7f
Gian Zamora <gz2016@att.net>
2023-01-30 20:24:46 -0800

    removed react-animate-on-scroll

--- src/components/MainHeader/mainheader.js
+++ src/components/MainHeader/mainheader.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './mainheader.css';
-import ScrollAnimation from 'react-animate-on-scroll';
 
 function MainHeader() {
-    return (
-        <div>
-            <div className='diagonal'>
-                <ScrollAnimation animateIn='name-animation'>
-                    <h1><strong className='name'>
-                        Gian Zamora
-                    </strong></h1>
-                </ScrollAnimation>
-                <h4 className='content'>Full Stack Web Developer</h4>
-            </div>
-        </div>
-    )
-};
+	return (
+		<div>
+			<div className="diagonal">
+				<h1>
+					<strong className="name">Gian Zamora</strong>
+				</h1>
+				<h4 className="content">Full Stack Web Developer</h4>
+			</div>
+		</div>
+	);
+}
 
 export default MainHeader;
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,40 +1,51 @@
 import React from 'react';
 import './skills.css';
-import ScrollAnimation from 'react-animate-on-scroll';
 
 function Skills() {
-    return (
-        <div id='skills-wrapper'>
-            <ScrollAnimation animateIn='fade-in' >
-                <div class='cell small-3 text-center left' id='card'>
-                    <h3 id='skill-titles'><strong>Front End Strengths</strong></h3>
-                    <div id='centered-list-1'>
-                        <ul id='list'>
-                            <li><strong>Javascript</strong></li>
-                            <li><strong>HTML / CSS</strong></li>
-                            <li><strong>Material UI / Bootstrap</strong></li>
-                            <li><strong>Heroku</strong></li>
-                        </ul>
-                    </div>
-                </div>
-            </ScrollAnimation>
+	return (
+		<div id="skills-wrapper">
+			<div class="cell small-3 text-center left" id="card">
+				<h3 id="skill-titles">
+					<strong>Front End Strengths</strong>
+				</h3>
+				<div id="centered-list-1">
+					<ul id="list">
+						<li>
+							<strong>Javascript</strong>
+						</li>
+						<li>
+							<strong>HTML / CSS</strong>
+						</li>
+						<li>
+							<strong>Material UI / Bootstrap</strong>
+						</li>
+						<li>
+							<strong>Heroku</strong>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-            <ScrollAnimation animateIn='fade-in' >
-                <div class='cell small-3 text-center right' id='card' >
-                    <h3 id='skill-titles'><strong>Back End Strengths</strong></h3>
-                    <div id='centered-list-2'>
-                        <ul id='list'>
-                            <li><strong>Node</strong></li>
-                            <li><strong>Express</strong></li>
-                            <li><strong>SQL</strong></li>
-                        </ul>
-                    </div>
-                    
-                </div>
-            </ScrollAnimation>
-            
-        </div>
-    )
-};
+			<div class="cell small-3 text-center right" id="card">
+				<h3 id="skill-titles">
+					<strong>Back End Strengths</strong>
+				</h3>
+				<div id="centered-list-2">
+					<ul id="list">
+						<li>
+							<strong>Node</strong>
+						</li>
+						<li>
+							<strong>Express</strong>
+						</li>
+						<li>
+							<strong>SQL</strong>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default Skills;
